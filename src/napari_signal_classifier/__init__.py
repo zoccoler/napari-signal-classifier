8fc527b25f6dce501a839392e051d30f2f4efdb3
Marcelo Zoccoler <marcelozoc@gmail.com>
2024-08-15 15:48:16 +0200

    remove import of deleted functions

--- src/napari_signal_classifier/__init__.py
+++ src/napari_signal_classifier/__init__.py
@@ -1,10 +1,8 @@
 __version__ = "0.0.1"
 from ._sample_data import make_sample_data
 from ._widget import Napari_Train_And_Predict_Signal_Classifier
-from ._features import calculate_statistics, calculate_crossings, calculate_entropy
-from ._features import get_signal_features_table, get_wavelet_coefficients_features_table, get_signal_with_wavelets_features_table
+from ._features import get_signal_features
 from ._classification import train_and_predict_signal_classifier, train_signal_classifier, predict_signal_labels
-from ._utilities import plot_wavelet_coefficient_decomposition_levels, get_frequency_bands
 
 
 __all__ = (
