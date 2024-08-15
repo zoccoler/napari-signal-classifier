340dd73ac4e12782b2c3485e770e5d8929ae90d8
Marcelo Zoccoler <marcelozoc@gmail.com>
2024-08-15 15:52:52 +0200

    Remove wavelets from widget and implement tsfresh features calculation

--- src/napari_signal_classifier/_widget.py
+++ src/napari_signal_classifier/_widget.py
@@ -1,19 +1,17 @@
 from typing import TYPE_CHECKING
 
 from qtpy import uic
-from qtpy.QtCore import QEvent, QObject
-from qtpy.QtWidgets import QWidget, QListWidgetItem
+from qtpy.QtWidgets import QWidget
 from magicgui.widgets import ComboBox
 from pathlib import Path
 from cmap import Colormap
 
 from napari_signal_selector.interactive import InteractiveFeaturesLineWidget
-from napari_signal_selector.utilities import get_custom_cat10based_cmap_list
+from nap_plot_tools.cmap import get_custom_cat10based_cmap_list
 from napari_skimage_regionprops._parametric_images import relabel_with_map_array
 
-from napari_signal_classifier._classification import train_and_predict_signal_classifier
-from napari_signal_classifier._features import get_signal_with_wavelets_features_table
-from napari_signal_classifier._utilities import extract_numbers_with_template
+from napari_signal_classifier._classification import train_signal_classifier, predict_signal_labels
+from napari_signal_classifier._features import get_signal_features
 
 from napari.utils import notifications
 import napari
@@ -36,69 +34,42 @@ class Napari_Train_And_Predict_Signal_Classifier(QWidget):
         self.plotter = napari_plotter
         # load the .ui file from the same folder as this python file
         uic.loadUi(Path(__file__).parent / "./_ui/napari_train_and_predict_signal_classfier.ui", self)
-        # add magicgui widget to widget layout
-        self._labels_combobox = ComboBox(
-            choices=self._get_labels_layer_with_features,
-            label='Labels layer:',
-            tooltip='Select labels layer with features to train and predict',
-        )
-        self.viewer.layers.events.inserted.connect(self._labels_combobox.reset_choices)
-        self.viewer.layers.events.removed.connect(self._labels_combobox.reset_choices)
-        self.layout().insertWidget(0, self._labels_combobox.native)
-        self.installEventFilter(self)
 
-        # Set features options
-        self._features_options = ['statistics', 'crossings', 'entropy']
-        for choice in self._features_options:
-            item = QListWidgetItem(choice)
-            self._features_multi_select_widget.addItem(item)
-            # Set the items as selected
-            item.setSelected(True)
-
-        # Set wavelet options
-        self._wavelet_family_combobox.addItems(['db', 'sym', 'coif', 'bior', 'rbio', 'dmey', 'haar'])
-        self._wavelet_family_combobox.currentIndexChanged.connect(self._on_wavelet_family_change)
-        self._wavelet_family_combobox.setCurrentIndex(0)
-        # Set wavelet initial order options from the first family
-        self._on_wavelet_family_change(0)
-        # Start with 'db4'
-        self._wavelet_order_combobox.setCurrentIndex(3)
+        self.viewer.layers.events.inserted.connect(self._reset_combobox_choices)
+        self.viewer.layers.events.removed.connect(self._reset_combobox_choices)
 
         self._run_button.clicked.connect(self._run)
+        # Populate combobox if there are already layers
+        self._reset_combobox_choices()
 
-    def eventFilter(self, obj: QObject, event: QEvent):
-        if event.type() == QEvent.ParentChange:
-            self._labels_combobox.parent_changed.emit(self.parent())
-
-        return super().eventFilter(obj, event)
-
-    def _get_labels_layer_with_features(self, combo_box):
+    def _get_labels_layer_with_features(self):
         '''Get selected labels layer'''
         return [layer for layer in self.viewer.layers if isinstance(
             layer, napari.layers.Labels) and len(layer.features) > 0]
 
-    def _on_wavelet_family_change(self, index):
-        '''Update wavelet order options'''
-        import pywt
-        wavelet_family = self._wavelet_family_combobox.currentText()
-        available_wavelet_orders = pywt.wavelist(wavelet_family)
-        wavelet_order_list = extract_numbers_with_template(available_wavelet_orders, wavelet_family)
-        self._wavelet_order_combobox.clear()
-        self._wavelet_order_combobox.addItems([str(order) for order in wavelet_order_list])
+    def _get_layer_by_name(self, layer_name):
+        '''Get layer by name'''
+        return [layer for layer in self.viewer.layers if layer.name == layer_name][0]
+
+    def _reset_combobox_choices(self):
+        # clear pyqt combobox choices
+        self._labels_layer_combobox.clear()
+        labels_layers = self._get_labels_layer_with_features()
+        # Set choices in qt combobox
+        self._labels_layer_combobox.addItems([layer.name for layer in labels_layers])
+        # Link layer name change event to this method
+        for layer in labels_layers:
+            # Clear previous connections
+            layer.events.name.disconnect()
+            layer.events.name.connect(self._reset_combobox_choices)
 
     def _run(self):
         if self.plotter is None:
             print('Plotter not found')
             notifications.show_warning('Plotter not found')
             return
-        selected_items = self._features_multi_select_widget.selectedItems()
-        selected_features = []
-        if selected_items:
-            selected_features = [item.text() for item in selected_items]
 
         classifier_path = self._classifier_path_line_edit.text()
-        waveletname = self._wavelet_family_combobox.currentText() + self._wavelet_order_combobox.currentText()
-        include_orignal_signal = self._include_original_signal_checkbox.isChecked()
         annotations_column_name = 'Annotations'
 
         # Check if plotter has data
@@ -110,36 +81,49 @@ class Napari_Train_And_Predict_Signal_Classifier(QWidget):
             x_column_name = self.plotter.x_axis_key
             object_id_column_name = self.plotter.object_id_axis_key
         # Get table from selected layer features
-        table = self._labels_combobox.value.features
+        table = self._get_layer_by_name(
+            self._labels_layer_combobox.currentText()).features
+        
+        # Get signal features table
+        signal_features_table = get_signal_features(
+            table, column_id=object_id_column_name,
+            column_sort=x_column_name,
+            column_value=y_column_name)
+        labels_data = self._get_layer_by_name(
+            self._labels_layer_combobox.currentText()).data
+        # Add signal features table as a new labels layer
+        self.viewer.add_labels(labels_data, name='Labels Layer with Signal Features', features=signal_features_table, visible=False)
 
-        table, classifier_path = train_and_predict_signal_classifier(
+        # Train signal classifier
+        clssifier_path = train_signal_classifier(
             table,
             classifier_path,
-            features_names=selected_features,
-            include_original_signal=include_orignal_signal,
-            waveletname=waveletname,
+            x_column_name=x_column_name,
             y_column_name=y_column_name,
-            annotations_column_name=annotations_column_name)
+            object_id_column_name=object_id_column_name,
+            annotations_column_name=annotations_column_name
+        )
+        # Get absolute path and set it to string
+        clssifier_path = Path(clssifier_path).absolute().as_posix()
+        self._classifier_path_line_edit.setText(clssifier_path)
+        # Run predictions
+        table_with_predictions = predict_signal_labels(
+            table,
+            classifier_path=clssifier_path,
+            x_column_name=x_column_name,
+            y_column_name=y_column_name,
+            object_id_column_name=object_id_column_name,
+            signal_features_table=signal_features_table
+        )
 
         # Make new_labels image where each label is replaced by the prediction number
-        labels_data = self._labels_combobox.value.data
-        label_list = table.groupby(object_id_column_name).mean().reset_index()[object_id_column_name].values
-        predictions_list = table.groupby(object_id_column_name).mean().reset_index()[
+        label_list = table_with_predictions.groupby(object_id_column_name).first().reset_index()[object_id_column_name].values
+        predictions_list = table_with_predictions.groupby(object_id_column_name).first().reset_index()[
             'Predictions'].values.astype('uint8')
         prediction_labels = relabel_with_map_array(labels_data, label_list, predictions_list)
         # Update table with predictions
-        self.viewer.layers.selection.active.features = table
-
-        # Display layers in napari
-        # Get signal features table
-        signals_table = table.pivot(
-            index=object_id_column_name,
-            columns=x_column_name,
-            values=y_column_name)
-        signal_features_table = get_signal_with_wavelets_features_table(
-            signals_table, waveletname, selected_features, include_orignal_signal)
-        # Add signal features table as a new labels layer
-        self.viewer.add_labels(labels_data, name='signal features', features=signal_features_table, visible=False)
+        self._get_layer_by_name(
+            self._labels_layer_combobox.currentText()).features = table_with_predictions
 
         # Generate predicionts labels layer
         prediction_cmap = Colormap(get_custom_cat10based_cmap_list()).to_napari()
@@ -155,8 +139,8 @@ class Napari_Train_And_Predict_Signal_Classifier(QWidget):
                 break
 
         # Select back the labels layer
-        # TODO: Identify labels layer in a different way
-        self.viewer.layers.selection.active = self.viewer.layers['labels']
+        self.viewer.layers.selection.active = self._get_layer_by_name(
+            self._labels_layer_combobox.currentText())
 
         # Re-plot with previous x_axis_key and y_axis_key
         self.plotter.y_axis_key = y_column_name
